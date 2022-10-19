f1d5362effd2f8f9a1aa69aaca0d4aadf4163921
chokabae <92rkqo@naver.com>
2022-10-19 14:43:14 +0900

    당첨번호 작업 - 랜덤 숫자 추출
    0.0~0.9999사이 숫자 추출

--- lotto.py
+++ lotto.py
@@ -3,8 +3,8 @@
 # 1. 6개의 숫자 입력받기 
 
 # 6개의 숫자를 담을 list를 생성
-from argparse import _MutuallyExclusiveGroup
-from re import M
+
+from random import random
 
 
 my_lotto_numbers = list()
@@ -66,6 +66,21 @@ for idx,val in enumerate(my_lotto_numbers):
             my_lotto_numbers[j+1] = backup
 
 print(my_lotto_numbers)
+
+
+# cpu가 숫자 6개 당첨 작업 진행
+# 6개의 숫자가 1~45의 범위, 중복 없어야 함
+
+# 당첨 번호 목록 
+win_number_list = list()
+
+for i in range(6):
+    # 사용할 수 있는 번호가 나올때 까지 무한 반복
+    while True :# random.random() > 0.0 ~ 0.9999의 랜덤값 출현
+        rand_number = random()
+
+
+
 
         
     
