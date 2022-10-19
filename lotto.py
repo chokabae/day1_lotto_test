27e4cfb495ad3c2d9413ef8916110f3b341b43c2
chokabae <92rkqo@naver.com>
2022-10-19 14:06:05 +0900

    제대로 된 입력이 올 때 까지 반복하는 구조
    - 범위 검사 수행 준비

--- lotto.py
+++ lotto.py
@@ -7,5 +7,19 @@ my_lotto_numbers = list()
 
 # 6번 숫자를 입력 : for
 for i in range(6):
-    input_num = int(input(f'{i+1}번째 로또 번호 입력 : '))
+    # 정수 입력 : 조건에 맞는 숫자가 입력될 때 까지 반복 입력
+    # 몇번 입력해야 ok인지 알 수 없다 -> while True : 사용
+
+    while True :
+        input_num = int(input(f'{i+1}번째 로또 번호 입력 : '))
+
+        #받아낸 input_num가 제대로 되었다면? 무한반복 종료
+        #반복 종료 조건 1~45 이내?
+
+        is_range_ok = True #임시로 괜찮다고 설정
+
+        if is_range_ok:
+            break
+
+        
     
