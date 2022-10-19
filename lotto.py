f32581ae6f021f40f7290b0a4394959cc13db36c
chokabae <92rkqo@naver.com>
2022-10-19 13:50:26 +0900

    6개의 숫자 입력

--- lotto.py
+++ lotto.py
@@ -1,3 +1,11 @@
 # 응용문제 7. 로또 맞춰보기 예제 
 
 # 1. 6개의 숫자 입력받기 
+
+# 6개의 숫자를 담을 list를 생성
+my_lotto_numbers = list()
+
+# 6번 숫자를 입력 : for
+for i in range(6):
+    input_num = int(input(f'{i}번째 로또 번호 입력 : '))
+    
